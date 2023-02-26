0fef5650f5f5acc901329faa9ef064f4e4d0a5d4
oyenet1 <bowofadeoyerinde@gmail.com>
2023-02-26 20:53:03 +0100

    Hello world in js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// console.log('I am javascript');
+// console.log('Second line in the queue');
+
+let tagName = "A new work in js values by manipulating";
+
+document.getElementById('demo').innerHTML = tagName;
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+console.log('I am javascript');
+// console.log('Second line in the queue');
